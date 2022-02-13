2e71314989549914fcb4c3f715bc4dad08b5cea6
Oddysey <98708913+Oddysey@users.noreply.github.com>
2022-02-12 19:58:06 -0600

    Add files via upload

---
+++ SuperAdvancedAI.py
@@ -0,0 +1,4 @@
+usrName = input("AI: What is your name: ")
+usrAge = input("AI: How old are you: ")
+usrHobby = input("AI: What do you like to do: ")
+superAdvancedAI = print("AI: Hello, " + usrName + " who is " + usrAge + " years old and, likes to " + usrHobby)
